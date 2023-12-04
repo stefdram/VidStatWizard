80ce6148e09b75934923ae1573d6a5a0119b03c0
Saksham Jain <SakshamJain@wirelessprv-10-193-243-253.near.illinois.edu>
2023-12-03 19:20:35 -0600

    title search backend

--- backend/route.js
+++ backend/route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./user-query');
+const Video= require('./video_titles');
 
 router.get('/users', User.getAllUsers);
+router.get('/users/video_titles', Video.video_titles);
 router.post('/create/user', User.createUser);
 router.delete('/delete/:userid', User.deleteUser);
 router.put('/update/user', User.updateUser);
---
+++ backend/video_titles.js
@@ -0,0 +1,17 @@
+const db = require("./model");
+
+// Read GET
+video_titles = async (req, res) => {
+    const search_key = req.body;
+  const query = "SELECT Title , ThumbnailLink , Tags FROM Video WHERE Title LIKE "%search_key%" LIMIT 10;";
+  db.query(query, (err, result) => {
+    // return res.status(200).send(result);
+    if (err) {
+      console.error("Error executing query:", err);
+      return res.status(500).send("Error fetching users");
+    }
+    return res.status(200).send(result);
+  });
+};
+
+module.exports = { video_titles }
